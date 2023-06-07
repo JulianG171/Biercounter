31c6d51bddcd66289cde0a5613d6a980ba861ad2
JulianG171 <134494878+JulianG171@users.noreply.github.com>
2023-06-07 16:12:56 +0200

    Schreiben/Lesen über txt-Datei hinzugefügt.

--- Biercounter.py
+++ Biercounter.py
@@ -5,15 +5,20 @@
 # Version: 1.0
 # Funktion: Anzeigen der über zwei Taster eingelesenen gezapften Biermenge auf einem Bildschirm
 # Autor: MJulian Grundmann
-# Datum der letzte Änderung: 06.06.23
+# Datum der letzte Änderung: 07.06.23
 # ------------------------------------------------------
 
 import RPi.GPIO as GPIO
 import tkinter as tk
 import time
 
-#Variable zum Zählen der Biermenge
-Biercounter = 0
+#test
+#Datei für Biermenge initial mit 0 befüllen
+datei = open('/home/jgrundmann/Biermenge.txt','r+')
+datei.truncate(0)
+datei.write('0')
+datei.close()
+
 
 #Pin-Setup
 GPIO.setmode(GPIO.BCM)
@@ -29,7 +34,7 @@ root.title("Biercounter")
 
 # Erstellen der GUI-Labels
 bierausschank = tk.Label(root, text = 'Biercounter: ', bg='purple', font=('times', 250, 'bold'))
-status_biermenge = tk.Label(root, text ='Bier', bg='purple', font=('times', 250, 'bold'))
+status_biermenge = tk.Label(root, text ='0', bg='purple', font=('times', 250, 'bold'))
 liter = tk.Label(root, text = ' Liter', bg='purple', font=('times', 250, 'bold'))
 
 #Platzieren der GUI-Labels
@@ -37,20 +42,32 @@ bierausschank.place(x=50, y=150)
 status_biermenge.place(x=150, y=600)
 liter.place(x=950, y=600)
 
-#CallbackFunktion, die auf Input-Pins schaut und Label für Biermenge aktualisiert
+#CallbackFunktion, die auf Input-Pins schaut und Wert in Datei sowie Label für Biermenge aktualisiert
 def update_bier():
-    global Biercounter
+    datei = open('/home/jgrundmann/Biermenge.txt','r+')
+    biermenge = int(datei.read())
+    print(biermenge)
+    datei.close()
+   
+    #global Biercounter
     if GPIO.input(23) == 0:
-        Biercounter+=50
-        status_biermenge["text"] = Biercounter
+        biermenge+=50
+        datei = open('/home/jgrundmann/Biermenge.txt','r+')
+        datei.truncate(0)
+        datei.write(str(biermenge))
+        datei.close()
         time.sleep(1) #Entprellen des Tasters und Vermeidung von zu schnellem Hochzählen
-               
+        
     if GPIO.input(24) == 0:      
-        if Biercounter > 0:
-            Biercounter-=50
-            status_biermenge["text"] = Biercounter
-            time.sleep(1) #Entprellen des Tasters und Vermeidung von zu schnellem Korrigieren
-             
+        if biermenge > 0:  # Keine negativen Werte möglich
+            biermenge-=50
+            datei = open('/home/jgrundmann/Biermenge.txt','r+')
+            datei.truncate(0)
+            datei.write(str(biermenge))
+            datei.close()
+        time.sleep(1) #Entprellen des Tasters und Vermeidung von zu schnellem Hochzählen
+            
+    status_biermenge["text"] = biermenge       
     root.after(1, update_bier) #rekursiver Aufruf der Funktion
     
 root.after(1, update_bier) #Aufruf der rekursiven update_bier-Funktion
